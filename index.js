dc483566f4be065d6c4f32fa385dfc42ea55c252
zinebhasni12 <zinebhasni.2011@gmail.com>
2023-11-03 16:46:25 +0100

    return the sum of two numbers

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 // Convert Minutes into Seconds
 minutes*60
 return result
+// Return the Sum of Two Numbers
+fontion 
+a+b
+return result 
+//Convert Age to Days
+age=x*y
+let x= 20; y= 365;
+return age
